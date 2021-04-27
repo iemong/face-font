b4d89fd18d989513ad175a36d98b8ef24c876d8d
iemong <kiyoshidainagon@gmail.com>
2021-04-27 23:35:18 +0900

    　videoをcanvas要素に表示する

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,12 +1,15 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+const WIDTH = 1280
+const HEIGHT = (WIDTH * 9) / 16
 
 const Home = (): JSX.Element => {
-    const init = async () => {
+    const initVideo = async () => {
         const constraints: MediaStreamConstraints = {
             audio: false,
             video: {
-                width: window.innerWidth,
-                height: window.innerHeight,
+                width: WIDTH,
+                height: HEIGHT,
             },
         }
         const stream = await navigator.mediaDevices.getUserMedia(constraints)
@@ -18,11 +21,47 @@ const Home = (): JSX.Element => {
         }
     }
 
+    const [context, setContext] = useState<CanvasRenderingContext2D | null>(
+        null
+    )
+
+    const initCanvas = () => {
+        const $canvas = canvasRef.current
+        if (!$canvas) return
+        $canvas.width = WIDTH
+        $canvas.height = HEIGHT
+        const context = $canvas.getContext('2d')
+        setContext(context)
+    }
+
+    const updateCanvas = () => {
+        console.log(context, videoRef.current)
+        if (!context || !videoRef.current) return
+        context.drawImage(videoRef.current, 0, 0, WIDTH, HEIGHT)
+    }
+
+    const loop = () => {
+        updateCanvas()
+        requestAnimationFrame(loop)
+    }
+
+    useEffect(() => {
+        if (!context) return
+        loop()
+    }, [context, loop])
+
+    const init = async () => {
+        await initVideo()
+        initCanvas()
+    }
+
     const videoRef = useRef<HTMLVideoElement | null>(null)
+    const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
     return (
         <>
-            <video ref={videoRef} />
+            <video ref={videoRef} className={'hidden'} />
+            <canvas ref={canvasRef} />
             <button onClick={init}>初期化</button>
         </>
     )
