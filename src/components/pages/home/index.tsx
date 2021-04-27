d0fe011205402b9e5051ad9b9c07659a76309a64
iemong <kiyoshidainagon@gmail.com>
2021-04-27 23:13:07 +0900

    getUserMedia使う

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,48 +1,30 @@
-import React, { useState } from 'react'
-import { useRecoilState, useRecoilValue } from 'recoil'
-import logo from '~/assets/images/logo.svg'
-import styles from './home.module.css'
-import { charCountState, textState } from '~/recoil/atoms/text'
-import Button from '~/components/atoms/Button'
-import { Link } from 'react-router-dom'
+import React, { useRef } from 'react'
 
 const Home = (): JSX.Element => {
-    const [count, setCount] = useState(0)
-    const [text, setText] = useRecoilState(textState)
-    const textLength = useRecoilValue(charCountState)
-
-    const handleChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setText(event.target.value)
+    const init = async () => {
+        const constraints: MediaStreamConstraints = {
+            audio: false,
+            video: {
+                width: window.innerWidth,
+                height: window.innerHeight,
+            },
+        }
+        const stream = await navigator.mediaDevices.getUserMedia(constraints)
+        const $video = videoRef.current
+        if (!$video) return
+        $video.srcObject = stream
+        $video.onloadedmetadata = () => {
+            $video.play()
+        }
     }
 
+    const videoRef = useRef<HTMLVideoElement | null>(null)
+
     return (
-        <div className="text-center">
-            <header className={styles.header}>
-                <img
-                    src={logo}
-                    className="h-96 pointer-events-none"
-                    alt="logo"
-                />
-                <p>Hello Vite + React!</p>
-                <p>
-                    <Link to="/about">About Page</Link>
-                </p>
-                <p>
-                    <Button onClick={() => setCount((count) => count + 1)}>
-                        count is: {count}
-                    </Button>
-                </p>
-                <div>
-                    <input
-                        type="text"
-                        value={text}
-                        onChange={handleChangeText}
-                        className="text-black"
-                    />
-                    <p>Character Count: {textLength}</p>
-                </div>
-            </header>
-        </div>
+        <>
+            <video ref={videoRef} />
+            <button onClick={init}>初期化</button>
+        </>
     )
 }
 
