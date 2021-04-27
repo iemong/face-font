071b2eaf8aef2ef5a49018d7ac4bc7ea0b5ced05
iemong <kiyoshidainagon@gmail.com>
2021-04-28 00:15:10 +0900

    グレースケールのモザイクかける

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -34,10 +34,52 @@ const Home = (): JSX.Element => {
         setContext(context)
     }
 
+    const createGrayScaleMosaic = (
+        context: CanvasRenderingContext2D,
+        imageData: ImageData,
+        mosaicSize = 4
+    ) => {
+        for (let y = 0; y < HEIGHT; y += mosaicSize) {
+            for (let x = 0; x < WIDTH; x += mosaicSize) {
+                const cR = imageData.data[(y * WIDTH + x) * 4]
+                const cG = imageData.data[(y * WIDTH + x) * 4 + 1]
+                const cB = imageData.data[(y * WIDTH + x) * 4 + 2]
+
+                const g = Math.floor((cR + cG + cB) / 3)
+
+                context.fillStyle = `rgb(${[g, g, g].join(',')})`
+                context.fillRect(x, y, x + mosaicSize, y + mosaicSize)
+            }
+        }
+    }
+
+    const createMosaic = (
+        context: CanvasRenderingContext2D,
+        imageData: ImageData,
+        mosaicSize = 4
+    ) => {
+        for (let y = 0; y < HEIGHT; y += mosaicSize) {
+            for (let x = 0; x < WIDTH; x += mosaicSize) {
+                const cR = imageData.data[(y * WIDTH + x) * 4]
+                const cG = imageData.data[(y * WIDTH + x) * 4 + 1]
+                const cB = imageData.data[(y * WIDTH + x) * 4 + 2]
+
+                context.fillStyle = 'rgb(' + cR + ',' + cG + ',' + cB + ')'
+                context.fillRect(x, y, x + mosaicSize, y + mosaicSize)
+            }
+        }
+    }
+
+    const effectCanvas = (context: CanvasRenderingContext2D) => {
+        const imageData = context.getImageData(0, 0, WIDTH, HEIGHT)
+        createGrayScaleMosaic(context, imageData, 32)
+        // createMosaic(context, imageData, 32)
+    }
+
     const updateCanvas = () => {
-        console.log(context, videoRef.current)
         if (!context || !videoRef.current) return
         context.drawImage(videoRef.current, 0, 0, WIDTH, HEIGHT)
+        effectCanvas(context)
     }
 
     const loop = () => {
