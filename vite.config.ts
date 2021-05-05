e6bc81b5b4e6a44589bec3214834778a0d023b1c
iemong <kiyoshidainagon@gmail.com>
2021-05-05 23:26:18 +0900

    viteにenvの設定をする

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
+const env = loadEnv(
+    'mock', // mode
+    process.cwd(), // root
+    '' // prefix (defaults to "VITE_")
+)
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh()],
     define: {
-        'process.env': process.env,
+        'process.env': env,
     },
     resolve: {
         alias: [{ find: '~', replacement: '/src' }],
