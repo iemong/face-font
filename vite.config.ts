55d32589be598ef5622bbbe036db6178e2a434ec
iemong <kiyoshidainagon@gmail.com>
2021-05-10 23:56:46 +0900

    https対応

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     define: {
         'process.env': env,
     },
+    server: {
+        https: true,
+    },
     resolve: {
         alias: [{ find: '~', replacement: '/src' }],
     },
